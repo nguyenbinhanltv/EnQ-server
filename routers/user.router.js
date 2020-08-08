ab4fa23c1a29a9abbccdb7038cb49b5f7c067f20
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-08 22:48:27 +0700

    add user router

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const controller = require('../controllers/user.controller');
+
 const router = express.Router();
 
-router.get();
-router.post();
+router.get('/:userId', controller.getUser);
+router.post('/', controller.updateUser);
 
 module.exports = router;
