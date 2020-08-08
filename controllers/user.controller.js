c3bffec0bc5ce58fd5a277a15a5b52fcba7f64f7
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-08 22:38:12 +0700

    create user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,23 @@
+const admin = require('firebase-admin');
+const firebaseHelper = require('firebase-functions-helper/dist');
+
+const db = admin.firestore();
+const collectionName = 'users';
+
+module.exports.getUser = (req, res) => {
+  const userId = req.param.userId;
+  firebaseHelper
+  .firestore
+  .getDocument(db, collectionName, userId)
+  .then(doc => res.status(200).send(doc))
+  .catch(err => res.status(400).send(err));
+}
+
+module.exports.updateUser = (req, res) => {
+  const data = req.body;
+  firebaseHelper
+  .firestore
+  .updateDocument(db, collectionName, data.id, data)
+  .then(doc => res.status(200).send('Update user data sucessfully !!!'))
+  .catch(err => res.status(400).send(err));
+}
