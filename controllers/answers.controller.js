115dae81e9fe69769af8e5d42106116d463c03c2
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-11 21:59:18 +0700

    update answer api request

--- controllers/answers.controller.js
+++ controllers/answers.controller.js
@@ -21,3 +21,31 @@ module.exports.getAnswer = (req, res) => {
 module.exports.getAnswers = (req, res) => {
   
 }
+
+//Update 1 answer
+module.exports.updateAnswer = (req, res) => {
+  const answerId = req.params.answerId;
+  const data = new Answer(req.body);
+  firebaseHelper
+  .firestore
+  .updateDocument(db, collectionName, answerId, data)
+  .then(doc => res.status(200).send(`Update answer ${answerId} successfully !!!`))
+  .catch(err => res.status(400).send(err));
+}
+
+//Post 1 answer
+module.exports.addAnswer = (req, res) => {
+  let data = new Answer(req.body);
+  firebaseHelper
+  .firestore
+  .createNewDocument(db, collectionName, data)
+  .then(doc => {
+    data.id = doc.id;
+    firebaseHelper
+    .firestore
+    .updateDocument(db, collectionName, data.id, data)
+    .then(doc => res.status(200).send(`Add answer ${data.id} successfully !!!`))
+    .catch(err => res.status(400).send(err));
+  })
+  .catch(err => res.status(400).send(err));
+}
