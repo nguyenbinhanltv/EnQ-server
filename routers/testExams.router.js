fd67e0dfe7eaa227743ec54ce038ff8a04c6c93d
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-11 22:41:34 +0700

    update test-exam api request

--- routers/testExams.router.js
+++ routers/testExams.router.js
@@ -4,5 +4,8 @@ const controller = require('../controllers/testExams.controller');
 const router = express.Router();
 
 router.get('/:testExamId', controller.getTestExam);
+router.post('/', controller.addTestExam);
+router.patch('/:testExamId', controller.updateTestExam);
+router.delete('/:testExamId', controller.deleteTestExam);
 
 module.exports = router;
