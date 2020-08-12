f4f967610c38c0d218be66dc050e6f78e8c84048
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-12 16:25:43 +0700

    update port

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const testExamsRouter = require('./routers/testExams.router');
 const leadersRouter = require('./routers/leaders.router');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
