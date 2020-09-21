8300e53c3eb71729890de878d2d7ccdfe62cf0f4
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-21 16:35:54 +0700

    update and finish adminUser model

--- server/models/admin.model.ts
+++ server/models/admin.model.ts
@@ -8,3 +8,17 @@ export interface AdminUser {
   confirmPassword: string, // Type string, min 6, max 20 characters
   refreshToken?: string // refresh token from jwt
 }
+
+/**
+ * @example
+ */
+const adminUser: AdminUser = {
+  _id: "123456",
+  displayName: "An Nguyen Binh",
+  userName: "username",
+  email: "username@gmail.com",
+  phone: "0987654321",
+  password: "$2a$10$miEFnNIv29QKJHs3q6pgl.Pdg.aF7LTB9RvVFOI/fgKcwyKJLun/i",
+  confirmPassword: "$2a$10$miEFnNIv29QKJHs3q6pgl.Pdg.aF7LTB9RvVFOI/fgKcwyKJLun/i",
+  refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDA2NzE1NDI4NjQwMCwiX2lkIjoiMTIzNDU2IiwiZGlzcGxheU5hbWUiOiJBbiBOZ3V5ZW4gQmluaCIsInVzZXJOYW1lIjoidXNlcm5hbWUiLCJlbWFpbCI6InVzZXJuYW1lQGdtYWlsLmNvbSIsInBob25lIjoiMDk4NzY1NDMyMSIsImlhdCI6MTYwMDY3MTU0Mn0.-aYhqGgkjcbFDSJla-TchaDGSqbJ7KqvV6YHWXgdeuw"
+};
