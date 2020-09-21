ed9096154654d2c778f73e5ab236590987342afa
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-21 16:33:41 +0700

    update and finish user model

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,8 +1,20 @@
 export interface User {
   displayName: string, //string name
   email: string, //string gmail
-  id: string, //string
+  _id: string, //string
   photoURL: string, //string
-  rank: string, //string
-  point: string, //string
+  rank: number, //number
+  point: number, //number
 }
+
+/**
+ * @example
+ */
+const user: User = {
+  displayName: "Sol L",
+  email: "yebasolar@gmail.com",
+  _id: "PLBzeFCT0PR7i1SQYBkb",
+  photoURL: "https://lh3.googleusercontent.com/a-/AOh14GhLWdM4dY2PkM6RHZAZQF5BZmEnKkpegZGH99ub=s96-c",
+  rank: 0,
+  point: 0
+};
