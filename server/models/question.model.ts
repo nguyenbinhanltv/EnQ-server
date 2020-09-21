f7e8f763bd21477fc72e1577ca0cc48c8d193a15
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-21 16:17:25 +0700

    update and finish question model

--- server/models/question.model.ts
+++ server/models/question.model.ts
@@ -1,7 +1,34 @@
+import { AnswerType } from '../utils/enum';
+
+interface Answer {
+  A: string, //string
+  B: string, //string
+  C: string, //string
+  D: string, //string
+  correctAnswer: AnswerType, //A, B, C, D
+}
+
 export interface Question {
-  id: string, //string
+  _id: string, //string
   title: string, //string
   type: string, //string
   rank: string, //string
-  answer: Array<string>, //Array<Answer.id>
+  answer: Answer, //Array<Answer>
 }
+
+/**
+ * @example
+ */
+const question: Question = {
+  _id: "EMfVyJXLS4BZMiyZredm",
+  title: "Are there any flowers to the left of your house? _____________.",
+  type: "grammer",
+  rank: "easy",
+  answer: {
+    A: "Yes, there are",
+    B: " Yes, there is",
+    C: "No, there isn’t",
+    D: "",
+    correctAnswer: 0,
+  }
+};
