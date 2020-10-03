6e65086f8bb0ebf81b8024eb36e186fd38bc126c
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-10-03 17:21:46 +0700

    update tests routes

--- server/routes/tests/tests.routes.ts
+++ server/routes/tests/tests.routes.ts
@@ -1,24 +1,23 @@
-import {Router} from 'express'
-import * as testController from '../../controllers/testExam/testExam.controller'
+import { Router } from "express";
+import * as testController from "../../controllers/testExam/testExam.controller";
 
 class TestExamRoute {
-   public routes:Router;
+  public routes: Router;
 
-   constructor() {
-       this.routes = Router()
-       this._init()
-   }
+  constructor() {
+    this.routes = Router();
+    this._init();
+  }
 
-   _init(){
-        //admin
-        this.routes.get('/question',testController.getQuestionByID); // /question?id=123
-        this.routes.post('/add-question',testController.addQuestion);
-        this.routes.post('/edit-question',testController.editQuestionById);
-        this.routes.post('/delete-question',testController.deleteQuestionById);
-        //mobile
-        this.routes.get('/get-test-exam',testController.shuffleQuestion);
-
-   }
+  _init() {
+    //admin
+    this.routes.get("/question", testController.getQuestionByID); // /question?id=123
+    this.routes.post("/question", testController.addQuestion);
+    this.routes.post("/question", testController.editQuestionById);
+    this.routes.delete("/question", testController.deleteQuestionById);
+    //mobile
+    this.routes.get("/get-test-exam", testController.shuffleQuestion);
+  }
 }
 
-export const testExamRoutes = new TestExamRoute().routes
+export const testExamRoutes = new TestExamRoute().routes;
